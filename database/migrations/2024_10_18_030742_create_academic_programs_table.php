66cfed895d84614c6133047a32366b252561790f
ao627515 <ao627515@gmail.com>
2024-10-18 03:09:26 +0000

    "Made logo and abbreviation fields in universities table nullable"

--- database/migrations/2024_10_18_030742_create_academic_programs_table.php
+++ database/migrations/2024_10_18_030742_create_academic_programs_table.php
@@ -11,10 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('universities', function (Blueprint $table) {
+        Schema::create('academic_programs', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('logo')->nullable();
             $table->string('abbreviation')->nullable();
             $table->timestamps();
         });
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('universities');
+        Schema::dropIfExists('academic_programs');
     }
 };
