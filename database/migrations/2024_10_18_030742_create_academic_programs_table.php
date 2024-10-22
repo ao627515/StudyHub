4d8f3e88c3e7765971d5efae1039fae39ee900e1
ao627515 <ao627515@gmail.com>
2024-10-22 14:22:37 +0000

    maj

--- database/migrations/2024_10_18_030742_create_academic_programs_table.php
+++ database/migrations/2024_10_18_030742_create_academic_programs_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('academic_programs', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('abbreviation')->nullable();
+            $table->string('abb', 20)->nullable()->comment("abbreviation");
             $table->timestamps();
         });
     }
