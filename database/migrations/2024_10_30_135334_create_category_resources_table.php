597ef5321b2c7fe3550cea4502b4cf914c2adcac
ao627515 <ao627515@gmail.com>
2024-10-30 15:11:41 +0000

    maj

--- database/migrations/2024_10_30_135334_create_category_resources_table.php
+++ database/migrations/2024_10_30_135334_create_category_resources_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('category_resources', function (Blueprint $table) {
-            $table->id();
+            $table->string('name')->unique();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
