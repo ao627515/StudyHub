3265f6f18de9ee777b2ed73c44041aec36342289
ao627515 <ao627515@gmail.com>
2024-10-30 15:02:04 +0000

    resource

---
+++ database/migrations/2024_10_30_145334_create_resources_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resources', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('image_url')->nullable();
+            $table->string('category');
+            $table->foreignId('course_module_id')
+                ->constrained()
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->integer('version')->default(1);
+            $table->foreignId('resource_id')
+                ->nullable()
+                ->constrained('resources')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+            $table->date('publication_date')->nullable();
+            $table->string('file_type')->nullable();
+            $table->string('download_url')->nullable();
+            $table->float('file_size')->nullable();
+            $table->unsignedInteger('view_count')->default(0);
+            $table->unsignedInteger('download_count')->default(0);
+            $table->foreignId('created_by_id')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete()
+                ->cascadeOnUpdate();
+            $table->foreignId('deleted_by_id')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete()
+                ->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resources');
+    }
+};
