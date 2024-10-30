aa37ad2eba89759862b680c427163d850dbbb49c
ao627515 <ao627515@gmail.com>
2024-10-30 15:13:50 +0000

    maj

--- app/Models/CategoryResource.php
+++ app/Models/CategoryResource.php
@@ -9,4 +9,10 @@ class CategoryResource extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoryResourceFactory> */
     use HasFactory;
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at'
+    ];
 }
--- app/Models/Resource.php
+++ app/Models/Resource.php
@@ -9,4 +9,10 @@ class Resource extends Model
 {
     /** @use HasFactory<\Database\Factories\ResourceFactory> */
     use HasFactory;
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at'
+    ];
 }
