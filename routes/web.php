f449fd7b53221aeddf4fd07b0bb2faaba731c76f
ao627515 <ao627515@gmail.com>
2024-11-06 02:45:04 +0000

    "Update web.php routes: change redirect from 'admin/dashboard' to 'home', add require for 'public.php'"

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', 'admin/dashboard', 301);
+Route::redirect('/', 'home', 301);
 
 require __DIR__ . '/admin.php';
+require __DIR__ . '/public.php';
 require __DIR__ . '/admin_auth.php';
