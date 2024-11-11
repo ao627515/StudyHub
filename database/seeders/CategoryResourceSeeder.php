3a7f64f63deb49ca6db22b2c7ba77746183e4630
ao627515 <ao627515@gmail.com>
2024-11-11 09:34:03 +0000

    "Updated migration to create 'category_resourcescategory_resources' table and added data seeding for category resources."

--- database/seeders/CategoryResourceSeeder.php
+++ database/seeders/CategoryResourceSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class CategoryResourceSeeder extends Seeder
 {
@@ -12,6 +13,27 @@ class CategoryResourceSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('category_resources')->insert([
+            [
+                'name' => 'TD',
+                'description' => 'Travaux dirigés, exercices pratiques pour approfondir les concepts théoriques vus en cours.'
+            ],
+            [
+                'name' => 'TP',
+                'description' => 'Travaux pratiques permettant aux étudiants de manipuler et d’appliquer les concepts techniques dans un environnement contrôlé.'
+            ],
+            [
+                'name' => 'Cours',
+                'description' => 'Cours théoriques couvrant les notions fondamentales et avancées d’un sujet donné.'
+            ],
+            [
+                'name' => 'Recueil De Devoir',
+                'description' => 'Collection de devoirs précédents, utilisés pour la révision et la préparation des examens.'
+            ],
+            [
+                'name' => 'Livre',
+                'description' => 'Livres académiques et manuels de référence pour approfondir la compréhension des matières.'
+            ]
+        ]);
     }
 }
