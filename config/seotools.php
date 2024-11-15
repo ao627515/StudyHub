963553bd4d51f36718b13ab661feb745842e08e1
ao627515 <ao627515@gmail.com>
2024-11-15 05:00:09 +0000

    maj

--- config/seotools.php
+++ config/seotools.php
@@ -23,12 +23,12 @@ return [
          * Webmaster tags are always added.
          */
         'webmaster_tags' => [
-            'google'    => 'votre_google_verification_code',
-            'bing'      => 'votre_bing_verification_code',
-            'alexa'     => null,
-            'pinterest' => null,
-            'yandex'    => null,
-            'norton'    => null,
+            // 'google'    => 'votre_google_verification_code',
+            // 'bing'      => 'votre_bing_verification_code',
+            // 'alexa'     => null,
+            // 'pinterest' => null,
+            // 'yandex'    => null,
+            // 'norton'    => null,
         ],
 
         'add_notranslate_class' => true, // Ajoute la classe 'notranslate' pour le contenu que vous ne voulez pas traduire
@@ -40,7 +40,7 @@ return [
         'defaults' => [
             'title'       => 'StudyHub - Centralisation des Ressources Académiques',
             'description' => 'Accédez aux ressources académiques centralisées de divers établissements scolaires grâce à StudyHub. Plateforme collaborative pour le partage de connaissances.',
-            'url'         => null, // Utilise Url::current() pour définir l'URL actuelle
+            'url'         => env('APP_URL'), // Utilise Url::current() pour définir l'URL actuelle
             'type'        => 'website', // Type d'opengraph - site web
             'site_name'   => 'StudyHub',
             'images'      => ['/assets/global/svg/logo.svg', '/assets/global/img/banner.png'], // Image par défaut pour les partages sociaux
@@ -51,8 +51,8 @@ return [
          * The default values to be used by the twitter cards generator.
          */
         'defaults' => [
-            'card'        => 'summary_large_image', // Carte Twitter au format image large
-            'site'        => '@votre_compte_twitter', // Compte Twitter associé
+            // 'card'        => 'summary_large_image', // Carte Twitter au format image large
+            // 'site'        => '@votre_compte_twitter', // Compte Twitter associé
         ],
         'https://votre_domaine.com/images/studyhub_logo.png'
     ],
