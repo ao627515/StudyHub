18ec8e921e792ceda2a4f4e58bc09b377cd9fbbf
ao627515 <ao627515@gmail.com>
2024-11-20 06:57:58 +0000

    "Updated resources-layout.blade.php: rearranged and duplicated layout toggle buttons, swapped grid and list conditions in card-body"

--- resources/views/components/resources-layout.blade.php
+++ resources/views/components/resources-layout.blade.php
@@ -16,22 +16,27 @@
               </a>
           @else
               <a href="{{ request()->fullUrlWithQuery(['layout' => 'list']) }}"
-                  class="btn btn-sm {{ request('layout') === 'list' || request('layout') === null || !request()->has('layout') ? 'btn-primary' : 'btn-outline-primary' }}"
-                  title="Vue liste">
+                  class="btn btn-sm {{ request('layout') === 'list' ? 'btn-primary' : 'btn-outline-primary' }}
+                  title="Vue
+                  liste">
                   <i class="uil uil-list-ul"></i>
               </a>
 
+
               <a href="{{ request()->fullUrlWithQuery(['layout' => 'grid']) }}"
-                  class="btn btn-sm ms-2 {{ request('layout') === 'grid' ? 'btn-primary' : 'btn-outline-primary' }}"
+                  class="btn btn-sm ms-2 {{ request('layout') === 'grid' || request('layout') === null || !request()->has('layout') ? 'btn-primary' : 'btn-outline-primary' }}"
                   title="Vue en grille">
                   <i class="uil uil-apps"></i>
               </a>
           @endif
 
+
       </div>
 
       <div class="card-body">
-          @if (request('layout') == 'grid')
+          @if (request('layout') == 'list')
+              <x:resource-table :resources="$resources" />
+          @else
               <div class="row">
                   @foreach ($resources as $resource)
                       <div class="col-md-4 mb-4">
@@ -39,8 +44,6 @@
                       </div>
                   @endforeach
               </div>
-          @else
-              <x:resource-table :resources="$resources" />
           @endif
       </div>
   </div>
